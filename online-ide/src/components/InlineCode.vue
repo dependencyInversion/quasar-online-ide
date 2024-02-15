7d1189685e73a750e97d57e44e3b84b162e52dab
Jan W <wennmann@protonmail.com>
2024-02-15 10:30:02 +0100

    move exercises & instructions to drawer + add code styling

---
+++ online-ide/src/components/InlineCode.vue
@@ -0,0 +1,14 @@
+<!-- InlineCode.vue -->
+<template>
+    <span class="inline-code"><slot></slot></span>
+  </template>
+  
+  <style>
+  .inline-code {
+    background-color: #f5f5f5; /* Light grey background */
+    font-family: monospace; /* Monospace font for code */
+    padding: 2px 4px; /* Padding around the text */
+    border-radius: 4px; /* Rounded corners */
+    border: 1px solid #e0e0e0; /* Light grey border */
+  }
+  </style>
