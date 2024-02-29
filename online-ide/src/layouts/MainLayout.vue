5edd3c5aae6a8580e5de4a25c33579d7faf9f218
Jan W <wennmann@protonmail.com>
2024-02-29 15:57:51 +0100

    final fixes

--- online-ide/src/layouts/MainLayout.vue
+++ online-ide/src/layouts/MainLayout.vue
@@ -1,46 +1,37 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-      </q-toolbar>
-    </q-header>
+      <q-header elevated :class="bg-secondary">
+        <q-toolbar>
+          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+          <q-toolbar-title>Marketing-Academy</q-toolbar-title>
+        </q-toolbar>
+      </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
-      bordered
-    >
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+        v-model="drawer"
+        show-if-above
+        :width="200"
+        :breakpoint="500"
+        bordered
+        class="bg-grey-3"
+      >
+        <q-scroll-area class="fit">
+          <q-list>
+            <template v-for="(menuItem, index) in menuList" :key="index">
+              <q-item clickable :to="menuItem.to" :active="menuItem.to === $route.path" v-ripple>
+                <q-item-section avatar>
+                  <q-icon :name="menuItem.icon" />
+                </q-item-section>
+                <q-item-section>
+                  {{ menuItem.label }}
+                </q-item-section>
+              </q-item>
+              <q-separator :key="'sep' + index"  v-if="menuItem.separator" />
+            </template>
 
-        <!-- Direkte Verwendung von router-link statt EssentialLink Komponente -->
-        <q-item
-          v-for="link in essentialLinks"
-          :key="link.title"
-          clickable
-          v-ripple
-        >
-          <q-item-section avatar>
-            <q-icon :name="link.icon" />
-          </q-item-section>
-          <q-item-section>
-            <!-- Verwendung von router-link für SPA-Navigation -->
-            <router-link :to="{ name: link.name }">{{
-              link.title
-            }}</router-link>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -55,29 +46,24 @@ export default defineComponent({
   name: "MainLayout",
 
   setup() {
-    const leftDrawerOpen = ref(false);
-
-    const essentialLinks = ref([
-      {
-        title: "Home",
-        caption: "Start",
-        icon: "home",
-        name: "home", // Verwenden Sie den Routennamen
-      },
-      {
-        title: "Exercise",
-        caption: "Start Coding",
-        icon: "code",
-        name: "exercise", // Verwenden Sie den Routennamen
-      },
-    ]);
+    const menuList = [
+    {
+      icon: 'home',
+      label: 'Home',
+      separator: false,
+      to: "/"
+    },
+    {
+      icon: 'school',
+      label: 'Exercise',
+      separator: true,
+      to: "/exercise"
+    },
+  ]
 
     return {
-      essentialLinks,
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
+      drawer: ref(false),
+      menuList
     };
   },
 });
--- online-ide/src/pages/IndexPage.vue
+++ online-ide/src/pages/IndexPage.vue
@@ -1,119 +1,24 @@
 <template>
-  <q-layout>
-    <q-page-container>
-      <q-page class="" style="flex-grow: 1;">
-        <q-btn
-          :icon="drawerIcon"
-          @click="toggleExerciseDrawer"
-          position="top-left"
-          :style="{ position: 'absolute', zIndex: '999' }"
+  <q-page class="flex flex-center" style="height: 100vh">
+    <div class="text-center row">
+      <q-banner rounded class="bg-grey-2">
+        <q-img
+          src="~assets/icon.webp"
+          fit="fill"
+          style="width: 300px; height: 300px;"
         />
-        <IframeEmbed url="http://localhost:8081/?folder=/home/developer/playground" />
-      </q-page>
-    </q-page-container>
-    <q-drawer
-      v-model="exerciseDrawerOpen"
-      side="left"
-      :width="drawerWidth"
-    >
-      <q-list bordered class="rounded-borders">
-        <q-item class="flex flex-grow-1">
-        <q-card flat>
-          <q-card-section class="q-gutter-sm row items-center justify-between">
-            <div class="row items-center">
-              <q-icon name="lightbulb_outline" size="sm" />
-              <span class="q-ml-md text-h6">Excerise</span>
-            </div>
-          </q-card-section>
-
-          <q-separator inset />
-
-          <q-card-section>
-            Examine correlations of the new features.<br>
-            Now that we have our volume and datetime features, we want to check the correlations between our new features (stored in the <InlineCode>new_features</InlineCode> list) and the target (<InlineCode>5d_close_future_pct</InlineCode>) to see how strongly they are related. Recall pandas has the built-in <InlineCode>.corr()</InlineCode> method for DataFrames, and seaborn has a nice <InlineCode>heatmap()</InlineCode> function to show the correlations.
-          </q-card-section>
-        </q-card>
-      </q-item>
-      <q-item class="flex flex-grow-1">
-        <q-card flat>
-          <q-card-section class="q-gutter-sm row items-center justify-between">
-            <div class="row items-center">
-              <q-icon name="task_alt" size="sm" />
-              <span class="q-ml-md text-h6">Instructions</span>
-            </div>
-            <div class="q-gutter-sm row items-center">
-              <q-badge>
-                100 XP
-              </q-badge>
-            </div>
-          </q-card-section>
-
-          <q-separator inset />
-
-          <q-card-section>
-            <ul>
-              <li>Extend our <InlineCode>new_features</InlineCode> variable to contain the weekdays' column names, such as <InlineCode>weekday_1</InlineCode>, by concatenating the weekday number with the <InlineCode>'weekday_'</InlineCode> string.</li>
-              <li>Use Seaborn's <InlineCode>heatmap</InlineCode> to plot the correlations of <InlineCode>new_features</InlineCode> and the target, <InlineCode>5d_close_future_pct</InlineCode>.</li>
-            </ul>
-          </q-card-section>
-
-          <q-card-section>
-            <q-btn label="Take Hint (-30 XP)" color="primary" icon="lightbulb_outline" />
-          </q-card-section>
-          </q-card>
-        </q-item>
-      </q-list>
-    </q-drawer>
-  </q-layout>
+        
+        <h3>Willkommen bei der<br>Marketing-Academy</h3>
+        <h5>Entdecken Sie die Zukunft des Online-Lernens!</h5>
+        <q-btn label="Jetzt starten" color="secondary" to="exercise"/>
+      </q-banner>
+    </div>
+  </q-page>
 </template>
-
-<style>
-.q-badge {
-  background-color: rgb(252, 206, 9);
-  color: black;
-  font-weight: bold;
-  border-radius: 4px; /* Optional: for rounded corners */
-  padding: 4px 8px; /* Adjust padding as needed */
-}
-</style>
 
 <script>
 import { defineComponent } from 'vue'
-import IframeEmbed from 'src/components/IframeEmbed.vue'
-import InlineCode from 'src/components/InlineCode.vue';
-
 export default defineComponent({
-    name: 'IndexPage',
-    components: {IframeEmbed, InlineCode },
-    data() {
-    return {
-      exerciseDrawerOpen: false,
-      drawerIcon: 'navigate_next',
-      drawerWidth: 300 // You can set this to be 1/3 of the viewport width
-    }
-  },
-  methods: {
-    toggleExerciseDrawer() {
-      this.exerciseDrawerOpen = !this.exerciseDrawerOpen;
-      if (this.exerciseDrawerOpen) {
-        // Calculate 1/3 of the current viewport width
-        this.drawerWidth = window.innerWidth / 3;
-      }
-
-      this.drawerIcon = this.exerciseDrawerOpen ? 'navigate_before' : 'navigate_next';
-    }
-  }
+  name: 'IndexPage'
 })
 </script>
-
-<!--
-# Add the weekday labels to the new_features list
-new_features.extend(['weekday_' + str(i) for i in range(1, 5)])
-
-# Plot the correlations between the new features and the targets
-sns.heatmap(lng_df[new_features + ['5d_close_future_pct']].corr(), annot=True)
-plt.yticks(rotation=0)  # ensure y-axis ticklabels are horizontal
-plt.xticks(rotation=90)  # ensure x-axis ticklabels are vertical
-plt.tight_layout()
-plt.show()
--->
